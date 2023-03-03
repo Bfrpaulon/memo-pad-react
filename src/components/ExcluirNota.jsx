4a56a5bc2f794e9f182a31dc7ebfeb206f5def7c
Bfrpaulon <brunopaulon@outlook.com.br>
2023-03-03 16:20:08 +0000

    update

--- src/components/ExcluirNota.jsx
+++ src/components/ExcluirNota.jsx
@@ -4,8 +4,7 @@ const ExcluirNota = ({ nota, onExcluir }) => {
     return (
         <button
             className="btn btn-danger"
-            onClick={() => onExcluir(nota.id)}
-        >
+            onClick={() => onExcluir(nota.id)}>
             Excluir
         </button>
     );
