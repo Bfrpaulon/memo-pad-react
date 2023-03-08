867e1af018cc4453ddf4c46dc1825280f2d697fd
Bfrpaulon <brunopaulon@outlook.com.br>
2023-03-08 14:39:45 +0000

    create structure of project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import NoteList from "./components/NoteList";
+import NoteForm from "./components/NoteForm";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-3">
+            <NoteForm />
+          </div>
+          <div className="col-sm-9">
+            <NoteList />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
