deb6d0be98d802fb105ab04a56f52079c9989bb9
Bruno Paulon <brunopaulon@outlook.com.br>
2023-03-08 03:02:23 +0000

    delete 6 files and update 3 files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Nota from './components/Nota';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Nota />
+    <App />
   </React.StrictMode>
 );
 
